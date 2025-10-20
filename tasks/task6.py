873b550897e29a78255afeeefc9b347dd36f4870
student <student@corp.kpiot.ru>
2025-10-20 17:13:57 +0500

    10

--- tasks/task6.py
+++ tasks/task6.py
@@ -2,9 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-    a,b,c=map(int,input().split())
-    print(a**2==b**2+c**2) 
-   
+    a,b,c=map (int, input().split())
+    sides = sorted( [a,b,c])
+    result = sides[0]**2 + sides [1]**2 == sides[2]**2
+    print(result)   
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
